63727fa90d7a0758ffa478efc494c4d4a33856ca
Hazard Cookie <wwallace@ripple.com>
2022-11-06 17:37:55 -0500

    Router Deploy

--- projects/exchange-protocol/scripts/deploy.ts
+++ projects/exchange-protocol/scripts/deploy.ts
@@ -6,8 +6,11 @@ async function main() {
 
     console.log(`Account balance: ${await deployer.getBalance().toString()}`);
 
-    const CookiePair = await ethers.getContractFactory("wXRP");
-    const cookiePair = await CookiePair.deploy();
+
+    const factory = "0x0205a49e3fb8e740a37b97698ffd552473685988"
+    const wXRP = "0xd624e3934d489ae4a08673d73d6a17e7cc428d83"
+    const CookiePair = await ethers.getContractFactory("CookieRouter");
+    const cookiePair = await CookiePair.deploy(factory, wXRP);
 
     console.log(`Token address: ${cookiePair.address}`);
 }
